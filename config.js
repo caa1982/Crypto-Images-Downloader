b8946f070de6ce1a83e1dd3ed321db21b5e73aab
Cyril Alvarez <cyril@2getherbank.com>
2017-10-31 14:02:15 +0100

    changed config

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 var config = {};
 
-config.path = ""; // Change path (destination of the download)
-config.resolution = "" // You can change the image resolution from 16x16, 32x32, 64x63 or 128x128
+// Change path (destination of the download)
+config.path = ""; 
+
+// You can change the image resolution from 16x16, 32x32, 64x63 or 128x128
+config.resolution = ""; 
 
 module.exports = config;
