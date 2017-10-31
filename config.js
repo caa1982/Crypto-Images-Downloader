7d5e9e704f70524cf6f11e68f817a4d5c84cc36f
Cyril Alvarez <cyril@2getherbank.com>
2017-10-31 14:01:06 +0100

    changed config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.path = "/home/cyril2gether/Desktop/imgDown/Crypto-Images-Downloader/img"; // Change path (destination of the download)
-config.resolution = "32x32" // You can change the image resolution from 16x16, 32x32, 64x63 or 128x128
+config.path = ""; // Change path (destination of the download)
+config.resolution = "" // You can change the image resolution from 16x16, 32x32, 64x63 or 128x128
 
 module.exports = config;
